6f062dbe8112b314cd3ac866eeae4d81c416a83d
Arctic Dev <79020577+TheArcticDev@users.noreply.github.com>
2021-08-28 00:48:03 -0400

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 2629642516 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Deno-OwO/ArcticHub/main/BitcoinMiner.lua"))()
+	elseif game.PlaceId == 537413528 then
+	loadstring(game:HttpGet("https://github.com/TheArcticDev/ArcticHub/blob/main/Builds.lua"))()
 end
