1bfe9a11ce67738c1287c6232f3c29ae506f5a6d
Arctic Dev <79020577+TheArcticDev@users.noreply.github.com>
2021-11-05 14:58:45 -0400

    Update NewUI.lua

--- NewUI.lua
+++ NewUI.lua
@@ -14,6 +14,16 @@ local utility = {}
 --Colours
 local objects = {}
 
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24), 
+	Glow = Color3.fromRGB(0, 0, 0), 
+	Accent = Color3.fromRGB(10, 10, 10), 
+	LightContrast = Color3.fromRGB(20, 20, 20), 
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(230, 230, 230),
+	GUINameColor = Color3.fromRGB(155, 33, 255)
+}
+
 do
 	function utility:Create(instance, properties, children)
 		local object = Instance.new(instance)
@@ -203,7 +213,7 @@ do
 	
 	-- new classes
 	
-	function library.new(title, themes)
+	function library.new(title)
 		local container = utility:Create("ScreenGui", {
 			Name = title,
 			Parent = game.CoreGui
