7f43ec18017518c9ab6d8fa699a2e813b7f0e084
Votteq <79020577+TheArcticDev@users.noreply.github.com>
2022-03-21 19:26:49 -0400

    Update testBack.lua

--- testBack.lua
+++ testBack.lua
@@ -1,7 +1,9 @@
-local screengui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
-local videoframe = Instance.new("VideoFrame", screengui)
-videoframe.Visible = true
-videoframe.Size = UDim2.new(1,0,1,0)
-videoframe.Transparency = 0
-videoframe.Video = getsynasset("/VotteqHub/Music/video.webm")
-videoframe:Play()
+for i,v in pairs(game.Players:GetChildren()) do 
+  local screengui = Instance.new("ScreenGui", v.PlayerGui)
+  local videoframe = Instance.new("VideoFrame", screengui)
+  videoframe.Visible = true
+  videoframe.Size = UDim2.new(1,0,1,0)
+  videoframe.Transparency = 0
+  videoframe.Video = getsynasset("/VotteqHub/Music/video.webm")
+  videoframe:Play()
+end
